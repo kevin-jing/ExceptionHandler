8d41b81d5ba15fad1d1873aaf663c1f9104de835
kevin-jing <jingqf@hotmail.com>
2018-07-07 23:17:40 +0800

    Initial commit

---
+++ ExceptionHandler/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.System.Threading;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ExceptionHandler
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TriggerException_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadPoolTimer.CreateTimer(MonitorAsync, TimeSpan.FromSeconds(0));
+        }
+
+        private async void MonitorAsync(ThreadPoolTimer source)
+        {
+            await Task.Delay(1000);
+            Task t = App.Logger.WriteMessage("MonitorAsync - Haha!!!");
+            t.Wait(3000);
+            throw new ArgumentException("Parameter cannot be null", "original");
+        }
+    }
+}
